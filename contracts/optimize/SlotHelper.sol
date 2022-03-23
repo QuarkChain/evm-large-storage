afb3595a6baee6dccbf3ddb0716684d9a5114a3f
cyl19970726 <15258378443@163.com>
2022-03-23 18:06:58 +0800

    modify contracts/optimize/SlotHelper.sol : add internal

--- contracts/optimize/SlotHelper.sol
+++ contracts/optimize/SlotHelper.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 // 存储方法
 library SlotHelper{
-    uint constant ADDRBITLEN = 160;
+    uint internal constant ADDRBITLEN = 160;
 
     function putRaw(bytes32 key , bytes memory data)
         internal 
