f73177bd6a3bd84c42fa0674d0cc9138c9b212b8
cyl19970726 <15258378443@163.com>
2022-03-25 11:34:06 +0800

    add contracts/examples/OptimizedFlatDirectory.sol and contracts/examples/OptimizedFlatDirectoryTest.sol

---
+++ contracts/examples/OptimizedFlatDirectoryTest.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./OptimizedFlatDirectory.sol";
+
+contract OptimizedFlatDirectoryTest is OptimizedFlatDirectory {
+    function readNonView(bytes memory name)
+        public
+        returns (bytes memory, bool)
+    {
+        return _get(keccak256(name));
+    }
+
+    function readManual(bytes memory name) external returns (bytes memory) {
+        (bytes memory content, ) = _get(keccak256(name));
+        StorageHelper.returnBytesInplace(content);
+        return content; // will never reach here
+    }
+}
