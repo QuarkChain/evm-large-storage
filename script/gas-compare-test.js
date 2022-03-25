891f0f396e483f937ee399ef30eb53ca63dc72a5
cyl19970726 <15258378443@163.com>
2022-03-25 18:40:32 +0800

    add script to generate gas-compare-text

---
+++ script/gas-compare-test.js
@@ -0,0 +1,56 @@
+const { ethers } = require("hardhat");
+var fs = require("fs");
+const { BigNumber } = require("ethers");
+let printGas = function (size,r1,r2){
+    const context = `${size.toString()}byte          ${r1.gasUsed}          ${r2.gasUsed} \n`;
+    // console.log(context);
+    writeInFile(context);
+}
+
+let writeInFile = function (str){
+    fs.appendFileSync('GasCostCompare.txt', str,function(err){
+        if (err) {
+            return console.error(err)
+        }
+    })
+}
+
+let WriteCode = async function (size){
+
+    const value = [];
+    for(let i = 0; i < size ; i++){
+        value.push(1);
+    }
+   
+    let opfdFactory = await ethers.getContractFactory("OptimizedFlatDirectory");
+    let fdFactory = await ethers.getContractFactory("FlatDirectory");
+
+    let opfd = await opfdFactory.deploy()
+    await opfd.deployed()
+
+    let fd = await fdFactory.deploy()
+    await fd.deployed()
+
+    let tx1 = await fd.writeChunk("0x01",0,value)
+    let receipt1 =await tx1.wait(1)
+    // console.log("gasUsed:",receipt1.gasUsed.toString())
+
+    let tx2 = await opfd.writeChunk("0x01",0,value)
+    let receipt2 =await tx2.wait(1)
+
+    // console.log("gasUsed:",receipt2.gasUsed.toString())
+    printGas(size,receipt1,receipt2)
+}
+
+
+
+let run = async function(){
+    writeInFile("size          normal          optimize\n");
+
+    const MaxSize = 300;
+    for (let size=20;size<=MaxSize;size+=20){
+        await WriteCode(size);
+    }
+}
+
+run()
