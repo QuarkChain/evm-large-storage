107f47e72e2411d43e24f54c5dbf0d897d7cb426
cyl19970726 <15258378443@163.com>
2022-03-25 14:24:24 +0800

    add createOptimize In FlatDirectoryFactory

--- contracts/examples/FlatDirectoryFactory.sol
+++ contracts/examples/FlatDirectoryFactory.sol
@@ -3,12 +3,20 @@
 pragma solidity ^0.8.0;
 
 import "./FlatDirectory.sol";
+import "./OptimizedFlatDirectory.sol";
 
 contract FlatDirectoryFactory {
     event FlatDirectoryCreated(address);
 
     function create() public returns (address) {
         FlatDirectory fd = new FlatDirectory();
+        fd.changeOwner(msg.sender);
+        emit FlatDirectoryCreated(address(fd));
+        return address(fd);
+    }
+
+    function createOptimize() public returns (address) {
+        OptimizedFlatDirectory fd = new OptimizedFlatDirectory();
         fd.changeOwner(msg.sender);
         emit FlatDirectoryCreated(address(fd));
         return address(fd);
