3eb34e4ffd0280c42d407c2aea235d63e4570a3e
cyl19970726 <15258378443@163.com>
2022-03-30 15:27:28 +0800

    update gas compare script and data

--- script/gas-compare-test.js
+++ script/gas-compare-test.js
@@ -1,56 +1,54 @@
 const { ethers } = require("hardhat");
 var fs = require("fs");
 const { BigNumber } = require("ethers");
-let printGas = function (size,r1,r2){
-    const context = `${size.toString()}byte          ${r1.gasUsed}          ${r2.gasUsed} \n`;
-    // console.log(context);
-    writeInFile(context);
-}
+let printGas = function (size, r1, r2) {
+  const context = `${size.toString()}byte          ${r1.gasUsed}          ${
+    r2.gasUsed
+  } \n`;
+  // console.log(context);
+  writeInFile(context);
+};
 
-let writeInFile = function (str){
-    fs.appendFileSync('GasCostCompare.txt', str,function(err){
-        if (err) {
-            return console.error(err)
-        }
-    })
-}
-
-let WriteCode = async function (size){
-
-    const value = [];
-    for(let i = 0; i < size ; i++){
-        value.push(1);
+let writeInFile = function (str) {
+  fs.appendFileSync("GasCostCompare.txt", str, function (err) {
+    if (err) {
+      return console.error(err);
     }
-   
-    let opfdFactory = await ethers.getContractFactory("OptimizedFlatDirectory");
-    let fdFactory = await ethers.getContractFactory("FlatDirectory");
+  });
+};
 
-    let opfd = await opfdFactory.deploy()
-    await opfd.deployed()
+let WriteCode = async function (size) {
+  const value = [];
+  for (let i = 0; i < size; i++) {
+    value.push(1);
+  }
 
-    let fd = await fdFactory.deploy()
-    await fd.deployed()
+  let fdFactory = await ethers.getContractFactory("FlatDirectory");
 
-    let tx1 = await fd.writeChunk("0x01",0,value)
-    let receipt1 =await tx1.wait(1)
-    // console.log("gasUsed:",receipt1.gasUsed.toString())
+  let opfd = await fdFactory.deploy(220);
+  await opfd.deployed();
 
-    let tx2 = await opfd.writeChunk("0x01",0,value)
-    let receipt2 =await tx2.wait(1)
+  let fd = await fdFactory.deploy(0);
+  await fd.deployed();
 
-    // console.log("gasUsed:",receipt2.gasUsed.toString())
-    printGas(size,receipt1,receipt2)
-}
+  let tx1 = await fd.writeChunk("0x01", 0, value);
+  let receipt1 = await tx1.wait(1);
+  // console.log("gasUsed:",receipt1.gasUsed.toString())
 
+  let tx2 = await opfd.writeChunk("0x01", 0, value);
+  let receipt2 = await tx2.wait(1);
 
+  // console.log("gasUsed:",receipt2.gasUsed.toString())
+  printGas(size, receipt1, receipt2);
+};
 
-let run = async function(){
-    writeInFile("size          normal          optimize\n");
+let run = async function () {
+  writeInFile("size          normal          optimize\n");
 
-    const MaxSize = 300;
-    for (let size=20;size<=MaxSize;size+=20){
-        await WriteCode(size);
-    }
-}
+  const MaxSize = 300;
+  for (let size = 28; size <= MaxSize; size += 32) {
+    await WriteCode(size);
+  }
+};
 
-run()
+run();
