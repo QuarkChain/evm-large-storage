b7b2c2bc31a00b2aa3a8d7fd37cec61dada2ed8f
cyl19970726 <15258378443@163.com>
2022-04-07 15:24:57 +0800

    add more test case for contract/IncentivizedFlatKV.sol

--- test/Incentivized-flatdirectory-test.js
+++ test/Incentivized-flatdirectory-test.js
@@ -1,17 +1,24 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 const { BigNumber } = require("ethers");
+const { ethers } = require("hardhat");
+const { refund } = require("w3q-deployer");
 const {writeChunkTest,writeTest,removeTest,removeChunkTest,readTest} = require("./shared/utils");
+
+let ETH = BigNumber.from(10).pow(18)
+let ChunkSize = BigNumber.from(1024)
+let CodeStakingPerChunk = BigNumber.from(10).pow(BigNumber.from(18))
 
 describe("IncentivizedFlatKV Test", function () {
   let OPFlatDirectory;
   let owner;
   let operator;
   let user;
+  let sendedEth;
   beforeEach(async () => {
     [owner,operator,user] = await ethers.getSigners();
     factory = await ethers.getContractFactory("IncentivizedFlatKV");
-    OPFlatDirectory = await factory.deploy(220);
+    sendedEth = BigNumber.from(10).pow(BigNumber.from(19));
+    OPFlatDirectory = await factory.deploy(220,ChunkSize,CodeStakingPerChunk,{value: sendedEth});
     await OPFlatDirectory.deployed();
   });
 
@@ -29,7 +36,7 @@ describe("IncentivizedFlatKV Test", function () {
     await expect(OPFlatDirectory.connect(operator.address).changeOwner(user.address)).to.be.reverted
   });
 
-  it("permission  test", async function () {
+  it("operator with different permissions test", async function () {
     expect(await OPFlatDirectory.owner()).to.eq(owner.address);
     await OPFlatDirectory.changeOperator(operator.address); 
     expect(await OPFlatDirectory.operator()).to.eq(operator.address);
@@ -52,5 +59,131 @@ describe("IncentivizedFlatKV Test", function () {
     await readTest(OPFlatDirectory.connect(user), "0x01",0,0,0,0,0,0)
 
   });
+
+  it("verify contract balance after write operation",async function(){
+    let totalTokenConsumed = BigNumber.from(0);
+
+    // verify FaatDirectory contract balance
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+    // set operator
+    await OPFlatDirectory.changeOperator(operator.address)
+
+    // need stake one eth 
+    let data0 = Array.from({ length: ChunkSize }, () =>
+      Math.floor(Math.random() * 256)
+    );
+    await OPFlatDirectory.connect(operator).write("0x01",data0)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth.sub(ETH))
+    totalTokenConsumed = totalTokenConsumed.add(ETH);
+
+    // need stake two eth 
+    let data1 = Array.from({ length: ChunkSize * 2}, () =>
+      Math.floor(Math.random() * 256)
+    );
+    await OPFlatDirectory.connect(operator).write("0x02",data1)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth.sub(ETH.mul(2)).sub(totalTokenConsumed))
+    totalTokenConsumed = totalTokenConsumed.add(ETH.mul(2))
+
+    // do not need stake 
+    let storageSlotCodeLen = await OPFlatDirectory.storageSlotCodeLength();
+    let v = await OPFlatDirectory.calValueForData(ChunkSize.sub(storageSlotCodeLen))
+    console.log("need value",v.toString())
+    expect(v).to.eq(BigNumber.from(0))
+
+    let data2 = Array.from({ length: ChunkSize.sub(storageSlotCodeLen) }, () =>
+      Math.floor(Math.random() * 256)
+    );
+    await OPFlatDirectory.connect(operator).write("0x03",data2)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth.sub(totalTokenConsumed))
+  })
+
+  it("write and remove test",async function(){
+    let totalTokenConsumed = BigNumber.from(0);
+
+    // verify FaatDirectory contract balance
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+    // set operator
+    await OPFlatDirectory.changeOperator(operator.address)
+
+    // need stake one eth 
+    let data0 = Array.from({ length: ChunkSize }, () =>
+      Math.floor(Math.random() * 256)
+    );
+    await OPFlatDirectory.connect(operator).write("0x01",data0)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth.sub(ETH))
+    totalTokenConsumed = totalTokenConsumed.add(ETH);
+
+    await OPFlatDirectory.connect(operator).remove("0x01")
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+  })
+
+  it("writeChunk and removeChunk test",async function(){
+    let totalTokenConsumed = BigNumber.from(0);
+
+    // verify FaatDirectory contract balance
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+    // set operator
+    await OPFlatDirectory.changeOperator(operator.address)
+
+    // need stake one eth 
+    let data0 = Array.from({ length: ChunkSize }, () =>
+      Math.floor(Math.random() * 256)
+    );
+    await OPFlatDirectory.connect(operator).writeChunk("0x01", 0, data0)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth.sub(ETH))
+    totalTokenConsumed = totalTokenConsumed.add(ETH);
+
+    await OPFlatDirectory.connect(operator).removeChunk( "0x01", 0)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+  })
+
+  it("writeChunk and removeChunk test",async function(){
+    let totalTokenConsumed = BigNumber.from(0);
+
+    // verify FaatDirectory contract balance
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+    // set operator
+    await OPFlatDirectory.changeOperator(operator.address)
+
+    // need stake one eth 
+    let data0 = Array.from({ length: ChunkSize }, () =>
+      Math.floor(Math.random() * 256)
+    );
+    await OPFlatDirectory.connect(operator).writeChunk("0x01", 0, data0)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth.sub(ETH))
+    totalTokenConsumed = totalTokenConsumed.add(ETH);
+
+    await OPFlatDirectory.connect(operator).removeChunk( "0x01", 0)
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+  })
+
+  it("refund with different permissions test",async function(){
+    // verify FaatDirectory contract balance
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+    // set operator
+    await OPFlatDirectory.changeOperator(operator.address)
+
+    // refund fail
+    await expect(OPFlatDirectory.connect(operator).refund()).to.be.reverted
+
+    // refund succeed
+    await OPFlatDirectory.connect(owner).refund()
+  })
+
+
+  it("destruct with different permissions test",async function(){
+    // verify FaatDirectory contract balance
+    expect(await OPFlatDirectory.balanceOf(OPFlatDirectory.address)).to.eq(sendedEth)
+    // set operator
+    await OPFlatDirectory.changeOperator(operator.address)
+
+    // refund fail
+    await expect(OPFlatDirectory.connect(operator).destruct()).to.be.reverted
+
+    // refund succeed
+    await OPFlatDirectory.connect(owner).destruct()
+  })
+
+
 
 });
