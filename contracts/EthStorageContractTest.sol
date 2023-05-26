699e8b3500a523035c8931f6cd6930cff184f124
iteyelmp@gmail.com <lmp19890127>
2023-05-26 17:37:04 +0800

    fix calldata can only upload 24K

--- contracts/EthStorageContractTest.sol
+++ contracts/EthStorageContractTest.sol
@@ -5,49 +5,35 @@ pragma solidity ^0.8.0;
 import "./LargeStorageManager.sol";
 
 contract EthStorageContractTest is LargeStorageManager(0) {
-    // Queue
-    mapping(uint256 => bytes32) queue;
-    uint256 first = 1;
-    uint256 last = 0;
 
-    function enqueue(bytes32 data) public {
-        queue[++last] = data;
-    }
-
-    function dequeue() public returns (bytes32 data) {
-        require(last >= first, "Queue is Empty");
-        data = queue[first];
-        delete queue[first++];
-    }
-
-
-
+    event PutBlob(bytes32 indexed key, uint256 indexed blobIdx, uint256 indexed length);
 
     // implement
     function putBlob(bytes32 key, uint256 blobIdx, uint256 length) external payable {
-        enqueue(key);
+        emit PutBlob(key, blobIdx, length);
     }
 
     // write real data
-    function writeChunk(bytes calldata data) public payable virtual {
-        bytes32 key = dequeue();
-        return _putChunkFromCalldata(key, 0, data, msg.value);
+    function writeBlobChunk(bytes32 key, uint256 chunkId, bytes calldata data) public payable virtual {
+        return _putChunkFromCalldata(key, chunkId, data, msg.value);
     }
 
     function remove(bytes32 key) external {
-        _removeChunk(key, 0);
+        for (uint256 i = _countChunks(key) - 1; i >= 0; i--) {
+            _removeChunk(key, i);
+        }
     }
 
     function get(bytes32 key, uint256 off, uint256 len) external view returns (bytes memory data) {
-        (data,) = _getChunk(key, 0);
+        (data,) = _get(key);
     }
 
     function size(bytes32 key) external view returns (uint256 s) {
-        (s,) = _chunkSize(key, 0);
+        (s,) = _size(key);
     }
 
     function hash(bytes32 key) external view returns (bytes24) {
-        (bytes memory localData,) = _getChunk(key, 0);
+        (bytes memory localData,) = _get(key);
         return bytes24(keccak256(localData));
     }
 
